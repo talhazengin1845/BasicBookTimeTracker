95f45fdbc150ab5cc0cb396c1ccc74fcf2db8f65
talhazengin1845 <talhazengin1845@gmail.com>
2024-02-27 22:26:03 +0300

    Book Tracker
    It is a web application for book day tracker

---
+++ WebApplicationProject/Controllers/BookController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using WebApplicationProject.Models;
+using Microsoft.VisualBasic;
+
+namespace WebApplicationProject.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: /<controller>/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Hello(string name, string author, int page_number, DateTime due_date, int a)
+        {
+            Book book = new Book();
+            book.bookName = name;
+            book.author = author;
+            book.pageNumber = page_number;
+            book.dueDate = due_date;
+            ///book.remainingTime = remainingTime;
+            DateTime now = DateTime.Now;
+            ///remainingTime = due_date - now;
+
+            TimeSpan remainingTime = due_date - now;
+            ///a = remainingTime.TotalDays;
+            book.remainingTime = remainingTime;
+            return View(book);
+        }
+    }
+}
---
+++ WebApplicationProject/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WebApplicationProject.Models
+{
+    public class Book
+    {
+        public string bookName { get; set; }
+        public string author { get; set; }
+        public int pageNumber { get; set; }
+        public DateTime dueDate { get; set; }
+        public TimeSpan remainingTime { get; set; }
+    }
+}
+
+
